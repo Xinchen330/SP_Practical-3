0f55f991dd04cdf35309c914c6adfda2e66bd44b
Xinchen330 <114107722+Xinchen330@users.noreply.github.com>
2022-11-10 14:16:06 +0000

    Add a file

---
+++ proj4.r
@@ -0,0 +1,13 @@
+# Group 12 Practical 2
+## Group Members: Xin Chen (s2340094)
+##                Yicong Sun (s2445309)
+##                Yihong Zhao (s2331659)
+
+## Address of the github repo: https://github.com/Xinchen330/SP_Practical-3.git
+
+# newt function
+newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
+                 maxit=100,max.half=20,eps=1e-6){
+  
+}
+
